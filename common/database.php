94e9fefd4be7f726247f420935bdc36aae793824
kamoto0427 <takahiro.08039@gmail.com>
2021-01-12 18:36:19 +0900

    ユーザー登録の仮完成

--- common/database.php
+++ common/database.php
@@ -5,7 +5,7 @@
       $database_handler = new PDO('mysql:host=db;dbname=simple_memo;charset=utf8mb4', 'root', 'password');
     }
     catch (PDOException $e) {
-      echo "DB接続に失敗しました。<br/>"
+      echo "DB接続に失敗しました。<br/>";
       echo $e->getMessage();
       exit;
     }
