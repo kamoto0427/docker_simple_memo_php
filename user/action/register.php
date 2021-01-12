2639fbc8825207339b3403cbca68050a8786cc60
kamoto0427 <takahiro.08039@gmail.com>
2021-01-12 18:07:53 +0900

    DBのSQL

--- user/action/register.php
+++ user/action/register.php
@@ -6,4 +6,19 @@
   $user_password = $_POST['user_password'];
 
   $database_handler = getDatabaseConnection();
+
+  try {
+    if($statement = $database_handler->prepare('INSERT INTO users (name, email, password) VALUES (:name, :email, :password)')) {
+      $password = password_hash($user_password, PASSWORD_DEFAULT);
+
+      $statement->bindParam(':name', htmlspecialchars($user_name));
+      $statement->bindParam(':email', $user_email);
+      $statement->bindParam(':password', $password);
+      $statement->execute();
+    }
+  }
+  catch (Throwable $e) {
+    echo $e->getMessage();
+    exit;
+  }
 ?>
